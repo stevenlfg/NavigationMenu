f089cec9f1f1a75a89031c4a349b5e3d7d41efe2
lifuguang <fuguang.li@519mi.com>
2015-07-16 09:55:32 +0800

    Initial Commit

---
+++ NavigationMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavigationMenu
+//
+//  Created by stevenlfg on 15/7/16.
+//  Copyright (c) 2015年 stevenlfg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NavigationMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavigationMenu
+//
+//  Created by stevenlfg on 15/7/16.
+//  Copyright (c) 2015年 stevenlfg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
